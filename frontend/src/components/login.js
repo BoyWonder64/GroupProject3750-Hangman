fd327b63ad0862c7cc73527be216c1a6c6edf30e
Winston Jo <122560578+winstonjo@users.noreply.github.com>
2024-07-12 11:28:19 -0600

    test 1

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,65 +1,56 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-export default function Login(){
-    const [form, setForm] = useState({
-        username: ""
+export default function Login () {
+  const [form, setForm] = useState({
+    username: ''
+  })
 
-    });
-    
-    const navigate = useNavigate();
+  const navigate = useNavigate()
 
-    const updateForm = e => {
-        const { name, value } = e.target
-        setForm(prevForm => ({...prevForm,[name]: value}))
+  const updateForm = e => {
+    const { name, value } = e.target
+    setForm(prevForm => ({ ...prevForm, [name]: value }))
+  }
+
+  const onSubmit = async e => {
+    e.preventDefault()
+    try {
+      const response = await fetch('http://localhost:4000/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
+        body: JSON.stringify(form)
+      })
+
+      if (response.ok) {
+        navigate('/hangman')
+      } else {
+        window.alert('An error occurred during the login process...')
+        setForm({ username: '' }) //clear
       }
-    
-
-     const onSubmit = async e => {
-        e.preventDefault();
-        // const newPerson = {...form};
-        const response =  await fetch("http://localhost:4000/login", {
-            method: "POST",
-            headers: {"Content-Type" : "application/json"},
-            credentials: 'include',
-            body: JSON.stringify(form),
-        })
-        .catch(error => {
-            window.alert(error);
-            return
-        });
-
-        if(response.ok)
-        { //message says its valid navigate to next page
-          
-           navigate("/hangman");
-        } else {
-            window.alert("An error occured during the login process...")
-                 setForm({username: ""}); //clear the form
-        }
-   
+    } catch (error) {
+      window.alert('An error occurred: ' + error)
     }
+  }
 
-    return (
-         <div>
-        <h2>Welcome to Hangman!</h2>
-        <h3>Please enter your username</h3>
-        <form onSubmit={onSubmit}>
-            <div>
-                <label>Username: </label>
-            <input
-                type="text"
-                name="username"
-                value={form.username}
-                onChange={updateForm}
-            />
-            </div>
-            <br/>
-            <input
-                type="submit"
-                value="Lets Play"
-            />
-        </form>
+  return (
+    <div>
+      <h2>Welcome to Hangman!</h2>
+      <h3>Please enter your username</h3>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label>Username: </label>
+          <input
+            type='text'
+            name='username'
+            value={form.username}
+            onChange={updateForm}
+          />
         </div>
-    );
-};
+        <br />
+        <input type='submit' value='Lets Play' />
+      </form>
+    </div>
+  )
+}
